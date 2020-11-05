cae6e8d3974880c0688235c7a8a8dc4f8939f872
Evan Mortimore <evan@madstyle.net>
2020-11-04 20:06:11 -0800

    Add more code: open dev and detach kernel driver.

--- test.cpp
+++ test.cpp
@@ -7,18 +7,23 @@
 
 using namespace std;
 
-void printdev(libusb_device *dev);
+bool printdev(libusb_device *dev);
+bool matchdev(libusb_device *dev);
 
 int main(void) {
-	libusb_device **devs;
-	libusb_context *ctx = nullptr;
-	int r;
-	size_t cnt;
+	libusb_device **devs{nullptr};
+	libusb_context *ctx{nullptr};
+	libusb_device_handle *dev{nullptr};
+	int r{0};
+	size_t cnt{0};
+	int idx{-1};
 
 	if ((r = libusb_init(&ctx)) < 0){
 		cerr << "Init Error" << r << endl;
 		return -1;
 	}
+
+	libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_INFO);
 
 	if((cnt = libusb_get_device_list(ctx, &devs)) <= 0) {
 		cerr << "Oops, no devices found" << endl;
@@ -27,8 +32,31 @@ int main(void) {
 	
 	cout << "There are " << cnt << " devices." << endl;
 
-	for (size_t i = 0; i < cnt; i++) {
-		printdev(devs[i]);
+	for (int i = 0; i < cnt; i++) {
+		if (matchdev(devs[i])) {
+			idx = i;
+			break;
+		}
+	}
+
+	if (idx >= 0) {
+		cout << "Idx is valid!" << endl;
+		printdev(devs[idx]);
+	}
+
+	// Okay, so now we know the index of our valid USB HID device.
+	if((dev = libusb_open_device_with_vid_pid(ctx, VID, PID)) == nullptr) {
+		cout << "Cannot open device" << endl;
+		return 0;
+	}
+
+	cout << "Device opened!" << endl;
+
+	if(libusb_kernel_driver_active(dev, 0) == 1) {
+		if(libusb_detach_kernel_driver(dev, 0) == 0) {
+			cout << "Kernel driver detached" << endl;
+			return 0;
+		}
 	}
 
 	libusb_exit(ctx);
@@ -37,19 +65,34 @@ int main(void) {
 
 void printconfig(libusb_config_descriptor*);
 
-void printdev(libusb_device *dev) {
+bool matchdev(libusb_device *dev) {
+	libusb_device_descriptor desc;
+	int r;
+
+	if( (r = libusb_get_device_descriptor(dev, &desc)) < 0) {
+		cout << "failed to get device descriptor" << endl;
+		return false;
+	}
+
+	if (PID != desc.idProduct || VID != desc.idVendor)
+		return false;
+	else
+		return true;
+}
+
+bool printdev(libusb_device *dev) {
 	libusb_device_descriptor desc;
 	libusb_config_descriptor *config;
 	int r;
 
 	if( (r = libusb_get_device_descriptor(dev, &desc)) < 0) {
 		cout << "failed to get device descriptor" << endl;
-		return;
+		return false;
 	}
 
 	// filter for VID/PID 
 	if (PID != desc.idProduct || VID != desc.idVendor)
-		return;
+		return false;
 
 	cout << "Num configs: " << (int)desc.bNumConfigurations << "   ";
 	cout << "Device Class: " << (int)desc.bDeviceClass << "   ";
@@ -59,10 +102,12 @@ void printdev(libusb_device *dev) {
 
 	if ( (r = libusb_get_config_descriptor(dev, 0, &config)) < 0) {
 		cout << "No Configuration Descriptor found" << endl;
-		return;
+		return false;
 	}
 
 	printconfig(config);
+
+	return true;
 }
 
 void printconfig(libusb_config_descriptor *config) {
@@ -70,9 +115,20 @@ void printconfig(libusb_config_descriptor *config) {
 	const libusb_interface_descriptor *iface_desc;
 	const libusb_endpoint_descriptor *ep_desc;
 
-	cout << "Interfaces: " << (int)config->bNumInterfaces << endl;
+	cout << "Interfaces: " << (int)config->bNumInterfaces << ", ";
 	for (int i=0; i<config->bNumInterfaces; i++) {
 		iface = &config->interface[i];
 		cout << "Alt settings: " << (int)iface->num_altsetting << endl;
+		for (int j=0; j<iface->num_altsetting; j++) {
+			iface_desc = &iface->altsetting[j];
+			cout << "    Interface#: " << (int)iface_desc->bInterfaceNumber << " | ";
+			cout << "#Endpoints: " << (int)iface_desc->bNumEndpoints << endl;
+			for (int k=0; k<iface_desc->bNumEndpoints; k++) {
+				ep_desc = &iface_desc->endpoint[k];
+				cout << "      Endpoint Type: " << (int)ep_desc->bDescriptorType << " | ";
+				cout << "Addr: " << (int)ep_desc->bEndpointAddress << endl;
+			}
+			cout << endl;
+		}
 	}
 }
